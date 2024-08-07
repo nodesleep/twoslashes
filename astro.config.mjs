d191520737f3e56bf018fb1e14a332971593adfc
Aron Sweet <sweet@andrews.edu>
2024-08-06 21:34:33 -0500

    Troubleshooting rewrite issue on Avatar loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()]
+  integrations: [tailwind(), icon(), mdx()],
+  site: "https://twoslashes.dev",
 });
